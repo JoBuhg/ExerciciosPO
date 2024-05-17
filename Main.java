ed3be0b3805d97092055144c86934965dc9f88c9
JoBuhg <126993913+JoBuhg@users.noreply.github.com>
2024-05-16 22:15:43 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package EXHerança.Progressões;
+
+public class Main {
+    public static void main(String[] args) {
+        Progressões a = new Progressões();
+        Fibonacci f = new Fibonacci();
+        Geometrica g =new Geometrica();
+
+        
+        a.Progressão(7, 4);
+        f.Progressão(7, 4);
+        g.Progressão(7, 4);
+    }
+}
